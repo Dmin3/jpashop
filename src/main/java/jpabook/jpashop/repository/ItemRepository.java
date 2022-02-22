19e248c09a6ba405a5a5db099ca9222029e906f4
Dmin3 <ekdrnzld@gmail.com>
2022-02-22 22:56:31 +0900

    Repository

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    //상품 등록
+    public void save(Item item) {
+        em.persist(item);
+    }
+
+    //상품조회
+    public Item find(Long itemId) {
+        return em.find(Item.class, itemId);
+    }
+
+    //상품 전체 조회
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+
+
+}
