5ccc0a978eea9d5f31995f5015da0643df91b8f1
Dmin3 <ekdrnzld@gmail.com>
2022-02-27 23:30:57 +0900

    item dto 추가

---
+++ src/main/java/jpabook/jpashop/web/dto/ItemSaveRequestDto.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.web.dto;
+
+import jpabook.jpashop.domain.item.Computer;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.item.Tv;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ItemSaveRequestDto {
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+    private String etc;
+    private String company;
+    private String dtype;
+
+
+
+
+
+
+
+}
