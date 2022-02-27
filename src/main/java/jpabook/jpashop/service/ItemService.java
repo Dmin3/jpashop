7602834e9a85925ef68272ee8d3848321729f615
Dmin3 <ekdrnzld@gmail.com>
2022-02-27 23:31:35 +0900

    item 카테코리 Check 메소드 추가

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,7 +1,11 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Computer;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.item.Phone;
+import jpabook.jpashop.domain.item.Tv;
 import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.web.dto.ItemSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,8 +20,30 @@ public class ItemService {
     private final ItemRepository itemRepository;
 
     @Transactional
-    public void saveItem(Item item){
+    public Long saveItem(ItemSaveRequestDto requestDto){
+
+        Item item = categoryCheck(requestDto);
+
+        item.setName(requestDto.getName());
+        item.setPrice(requestDto.getPrice());
+        item.setStockQuantity(requestDto.getStockQuantity());
+
         itemRepository.save(item);
+        return item.getId();
+    }
+
+    private Item categoryCheck(ItemSaveRequestDto requestDto) {
+
+        switch (requestDto.getDtype()){
+            case "T":
+               return new Tv(requestDto.getCompany(), requestDto.getEtc());
+            case "C":
+                return new Computer(requestDto.getCompany(), requestDto.getEtc());
+            case "P":
+                return new Phone(requestDto.getCompany(), requestDto.getEtc());
+
+        }
+        return null;
     }
 
     public List<Item> findItems() {
