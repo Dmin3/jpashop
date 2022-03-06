6ef45526e023387ef8dee3770802a9f6704522a0
Dmin3 <ekdrnzld@gmail.com>
2022-03-06 15:54:17 +0900

    장바구니 Repository

---
+++ src/main/java/jpabook/jpashop/repository/OrderItemRepository.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderItemRepository {
+
+    private final EntityManager em;
+
+    public void save(OrderItem orderItem){
+        em.persist(orderItem);
+    }
+
+    public OrderItem find(Long orderItemId) {
+        return em.find(OrderItem.class, orderItemId);
+    }
+
+    public List<OrderItem> findAll() {
+        return em.createQuery("select oi from OrderItem oi", OrderItem.class).getResultList();
+    }
+
+
+}
