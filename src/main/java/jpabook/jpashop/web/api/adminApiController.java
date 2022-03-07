4e542eac5b1769ee35b512a086828cca7d341c40
Dmin3 <ekdrnzld@gmail.com>
2022-03-07 23:50:51 +0900

    상품전체조회 API 추가

--- src/main/java/jpabook/jpashop/web/api/adminApiController.java
+++ src/main/java/jpabook/jpashop/web/api/adminApiController.java
@@ -1,11 +1,20 @@
 package jpabook.jpashop.web.api;
 
+import jpabook.jpashop.domain.item.Computer;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.web.dto.ItemFindAllResponseDto;
 import jpabook.jpashop.web.dto.ItemSaveRequestDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,13 +23,21 @@ public class adminApiController {
     private final ItemService itemService;
 
     //상품 등록
-    @PostMapping("/api/v1/itemSave")
+    @PostMapping("/api/v1/item-save")
     public Long save(@RequestBody ItemSaveRequestDto requestDto){
 
         return itemService.saveItem(requestDto);
 
 
     }
+
+    //상품 전체조회
+    @GetMapping("/api/v1/item-findAll")
+    public List<ItemFindAllResponseDto> ItemFindAll() {
+       return itemService.findItems();
+    }
+
+
 
 
 
