04b06002bb465006002fe895da1fda52771762ef
Dmin3 <ekdrnzld@gmail.com>
2022-03-07 23:51:07 +0900

    상품전체조회

--- src/main/java/jpabook/jpashop/web/controller/adminController.java
+++ src/main/java/jpabook/jpashop/web/controller/adminController.java
@@ -1,14 +1,35 @@
 package jpabook.jpashop.web.controller;
 
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.web.dto.ItemFindAllResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class adminController {
+
+    private final ItemService itemService;
+
 
     @GetMapping("/itemSave")
     public String itemSave() {
         return "/item/itemSave";
+    }
+
+    //상품전체조회
+    @GetMapping("/item")
+    public String findAllItem(Model model) {
+
+        List<ItemFindAllResponseDto> items = itemService.findItems();
+
+        model.addAttribute("item", items);
+
+        return "/admin/item";
     }
 
 
