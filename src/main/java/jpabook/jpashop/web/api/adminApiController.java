3a00879ad587b475367ef7036401c7a01f9d7e0d
Dmin3 <ekdrnzld@gmail.com>
2022-03-10 17:23:13 +0900

    admin 로그인 기능 추가

--- src/main/java/jpabook/jpashop/web/api/adminApiController.java
+++ src/main/java/jpabook/jpashop/web/api/adminApiController.java
@@ -1,42 +1,60 @@
 package jpabook.jpashop.web.api;
 
-import jpabook.jpashop.domain.item.Computer;
-import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.service.ItemService;
-import jpabook.jpashop.web.dto.ItemFindAllResponseDto;
-import jpabook.jpashop.web.dto.ItemSaveRequestDto;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderItemService;
+import jpabook.jpashop.web.dto.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class adminApiController {
 
     private final ItemService itemService;
+    private final MemberService memberService;
+    private final MemberRepository memberRepository;
+    private final OrderItemService orderItemService;
+
+    //admin 로그인
+    @PostMapping("/v1/admin-login")
+    public MemberResponseDto login(@RequestBody MemberRequestDto requestDto, HttpSession session){
+        if (memberService.login(requestDto)){
+            Member admin = memberRepository.findOneByName(requestDto.getName());
+            session.setAttribute("admin", admin);
+        } else {
+            throw new IllegalStateException("존재하지 않는 회원입니다");
+        }
+        Member member = (Member) session.getAttribute("admin");
+
+        return new MemberResponseDto(member.getId(), member.getName(), member.getAddress());
+    }
 
     //상품 등록
-    @PostMapping("/api/v1/item-save")
+    @PostMapping("/v1/item-save")
     public Long save(@RequestBody ItemSaveRequestDto requestDto){
 
         return itemService.saveItem(requestDto);
-
-
     }
 
     //상품 전체조회
-    @GetMapping("/api/v1/item-findAll")
+    @GetMapping("/v1/item-findAll")
     public List<ItemFindAllResponseDto> ItemFindAll() {
        return itemService.findItems();
     }
 
+    //주문상품 조회
+    @GetMapping("/v1/orderItem-findAll")
+    public List<OrderItemDto> orderItemFindAll() {
+        List<OrderItemDto> all = orderItemService.findAll();
+        return all;
+    }
 
 
 
