771ff0452df2083f1b6f410ca602dc678f2c3fab
Dmin3 <ekdrnzld@gmail.com>
2022-03-10 17:24:54 +0900

    필드 추가

--- src/main/java/jpabook/jpashop/web/dto/MemberResponseDto.java
+++ src/main/java/jpabook/jpashop/web/dto/MemberResponseDto.java
@@ -1,15 +1,20 @@
 package jpabook.jpashop.web.dto;
 
+import jpabook.jpashop.domain.Address;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
+@Data
 public class MemberResponseDto {
 
     private Long id;
+    private String name;
+    private Address address;
 
-    public MemberResponseDto(Long id) {
+    public MemberResponseDto(Long id, String name, Address address) {
         this.id = id;
+        this.name = name;
+        this.address = address;
     }
 }
